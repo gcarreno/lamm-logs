42e895c0a732566c6e1c920a2b6bd8b10d58f498
Gustavo Carreno <guscarreno@gmail.com>
2016-08-07 19:35:33 +0100

    Adding the DataModule

---
+++ src/lamm.logs.datamodules.main.pas
@@ -0,0 +1,28 @@
+unit LAMM.Logs.DataModules.Main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil;
+
+{ TdmMain }
+
+type
+  TdmMain = class(TDataModule)
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  dmMain: TdmMain;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
